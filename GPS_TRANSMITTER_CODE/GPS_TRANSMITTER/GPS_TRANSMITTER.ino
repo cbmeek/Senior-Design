aaba8af6266f228334df8b8f04605895d2277bb6
grrreen <christopherbmeek09@gmail.com>
2018-04-13 02:27:58 -0700

    Changed format of transmitted sentence

--- GPS_TRANSMITTER_CODE/GPS_TRANSMITTER/GPS_TRANSMITTER.ino
+++ GPS_TRANSMITTER_CODE/GPS_TRANSMITTER/GPS_TRANSMITTER.ino
@@ -35,9 +35,9 @@ void loop() {
   
   if(lat.isUpdated() || lon.isUpdated() || ele.isUpdated()){
     Serial1.print("<"); //Start char for parsing
-    Serial1.print(nmea2DD(lat.value(),NS.value()),9); Serial1.print(" "); 
-    Serial1.print(nmea2DD(lon.value(),EW.value()),9); Serial1.print(" ");
-    Serial1.print(ele.value()); Serial1.print(" ");
+    Serial1.print(nmea2DD(lat.value(),NS.value()),9); Serial1.print(","); 
+    Serial1.print(nmea2DD(lon.value(),EW.value()),9); Serial1.print(",");
+    Serial1.print(ele.value()); Serial1.print(",");
     Serial1.print(batteryVoltage());
     Serial1.println(">"); //End char for parsing
   }

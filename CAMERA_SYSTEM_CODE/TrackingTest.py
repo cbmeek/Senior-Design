ab30679a08d5fe5e6b9bcb6849ceb3902c80a8f7
grrreen <christopherbmeek09@gmail.com>
2018-04-23 21:26:42 +0000

    Computer Vision Tracking added

---
+++ CAMERA_SYSTEM_CODE/TrackingTest.py
@@ -0,0 +1,115 @@
+import numpy as np
+import cv2
+import boto3
+import json
+import sys
+from time import monotonic
+
+#initilize timer so I dont spend a fortune on AWS
+time = 0
+lastTime = 0
+
+# Create AWS client
+client=boto3.client('rekognition','us-west-2')
+
+# Create Tracker 
+tracker = cv2.TrackerMedianFlow_create()
+# Read video
+video = cv2.VideoCapture(0)
+     
+# Exit if video not opened.
+if not video.isOpened():
+    print ("Could not open video")
+    sys.exit()
+ 
+# Read first frame.
+ok, frame = video.read()
+if not ok:
+    print ('Cannot read video file')
+    sys.exit()
+    
+#Deinterlace frame    
+frame[1:-1:2] = frame[0:-2:2]/2 + frame[2::2]/2
+        
+# Get frame Dimensions
+frameHeight,frameWidth,dim=frame.shape
+
+#Convert frame into a usable data type for AWS Rekognition
+result,encimg=cv2.imencode('.jpg',frame,[int(cv2.IMWRITE_JPEG_QUALITY),100])
+if False==result:
+    print("could not encode image!")
+    quit()
+imgbytes = encimg.tostring()
+
+#send frame to AWS Rekognition
+response = client.detect_faces(Image={'Bytes': imgbytes}, Attributes=['DEFAULT'])
+for faceDetail in response['FaceDetails']:
+    #Getting bbox from AWS Rekoginiton
+    try:
+        rectVert1 = (int(frameWidth*faceDetail['BoundingBox']['Left']),int(frameHeight*faceDetail['BoundingBox']['Top']))
+        rectVert2 = (int(frameWidth*faceDetail['BoundingBox']['Width']),int(frameHeight*faceDetail['BoundingBox']['Height']))
+        # Define an initial bounding box
+        bbox = rectVert1+rectVert2
+        print(bbox)
+    except:
+        print("No face detected using default roi")
+        bbox = (287, 23, 86, 320)
+
+# Initialize tracker with first frame and bounding box
+ok = tracker.init(frame, bbox)
+ 
+while True:
+    time = monotonic()   
+    # Read a new frame
+    ok, frame = video.read()
+    frame[1:-1:2] = frame[0:-2:2]/2 + frame[2::2]/2
+    if not ok:
+        break
+         
+    # Start timer
+    timer = cv2.getTickCount()
+    
+    # Update tracker
+    ok, bbox = tracker.update(frame)
+ 
+    # Calculate Frames per second (FPS)
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+ 
+    # Draw bounding box
+    if ok:
+        # Tracking success
+        p1 = (int(bbox[0]), int(bbox[1]))
+        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+        cv2.rectangle(frame, p1, p2, (0,255,0), 3)
+    else :
+        # Tracking failure
+         cv2.putText(frame, "Tracking failure detected. Looking for new target.", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+         if time-lastTime>5:
+             #Convert frame into a usable data type for AWS Rekognition
+             result,encimg=cv2.imencode('.jpg',frame,[int(cv2.IMWRITE_JPEG_QUALITY),100])
+             if False==result:
+                 print("could not encode image!")
+                 quit()
+             imgbytes = encimg.tostring()
+             response = client.detect_faces(Image={'Bytes': imgbytes}, Attributes=['DEFAULT'])
+             for faceDetail in response['FaceDetails']:
+                 #Getting bbox from AWS Rekoginiton
+                 try:
+                     rectVert1 = (int(frameWidth*faceDetail['BoundingBox']['Left']),int(frameHeight*faceDetail['BoundingBox']['Top']))
+                     rectVert2 = (int(frameWidth*faceDetail['BoundingBox']['Width']),int(frameHeight*faceDetail['BoundingBox']['Height']))
+                     bbox = rectVert1+rectVert2
+                     ok = tracker.init(frame, bbox)
+                 except:
+                     print("No face detected using default roi")
+                     bbox = (frameWidth, frameHeight,frameWidth , frameHeight)
+             lastTime= monotonic()
+             
+    # Display FPS on frame
+    cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+ 
+    # Display result
+    cv2.imshow("Tracking", frame)
+ 
+    # Exit if ESC pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
